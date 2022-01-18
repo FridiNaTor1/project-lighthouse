f4834ed9fcef7e6833a84577d5ea8fa3ed489db5
FridiNaTor1 <32988388+FridiNaTor1@users.noreply.github.com>
2022-01-18 09:37:27 +0000

    Update UsersPage.cshtml.cs

--- ProjectLighthouse/Pages/UsersPage.cshtml.cs
+++ ProjectLighthouse/Pages/UsersPage.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using JetBrains.Annotations;
 using LBPUnion.ProjectLighthouse.Helpers;
 using LBPUnion.ProjectLighthouse.Pages.Layouts;
-using LBPUnion.ProjectLighthouse.Types.Levels;
+using LBPUnion.ProjectLighthouse.Types.Accounts;
 using LBPUnion.ProjectLighthouse.Types.Settings;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
