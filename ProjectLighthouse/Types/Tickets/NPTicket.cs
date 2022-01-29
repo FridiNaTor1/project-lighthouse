a18803912626e12eae9c41aa661f3fbb6edb6691
FridiNaTor1 <fridij2007@icloud.com>
2022-01-29 15:52:58 +0000

    Maybe a temporary fix for Vita

--- ProjectLighthouse/Types/Tickets/NPTicket.cs
+++ ProjectLighthouse/Types/Tickets/NPTicket.cs
@@ -106,7 +106,7 @@ public class NPTicket
             {
                 0x100 => Platform.PS3,
                 0x33333333 => Platform.RPCS3,
-                _ => Platform.Unknown,
+                _ => Platform.Vita,
             };
 
             if (npTicket.Platform == Platform.Unknown)
